1669954877731612ae2cf9b992da34fbb84eb889
ИльЯ Алипов <ilya12562@gmail.com>
2024-09-15 15:52:48 +0300

    make logic with likes posts and authors, modify models.py, add error.htm and static dir

--- models.py
+++ models.py
@@ -46,6 +46,37 @@ class ProjectModel(db.Model):
         db.String(LENGHT_TITLE),
         nullable=False
     )
+    likes_count = db.Column(
+        db.Integer,
+        default=0
+    )
+    likes = db.relationship(
+        'LikeModel',
+        back_populates='project'
+    )
+
+class LikeModel(db.Model):
+    __tablename__ = 'likes'
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    project_id = db.Column(
+        db.Integer,
+        db.ForeignKey('projects.id')
+    )
+    author_id = db.Column(
+        db.Integer,
+        db.ForeignKey('author.id')
+    )
+    project = db.relationship(
+        'ProjectModel',
+        back_populates='likes'
+    )
+    author = db.relationship(
+        'AuthorModel',
+        back_populates='likes'
+    )
 
 class AuthorModel(db.Model):
     __tablename__ = 'author'
@@ -63,4 +94,8 @@ class AuthorModel(db.Model):
     )
     token = db.Column(
         db.String(LENGHT_TOKEN)
+    )
+    likes = db.relationship(
+        'LikeModel',
+        back_populates='author'
     )
